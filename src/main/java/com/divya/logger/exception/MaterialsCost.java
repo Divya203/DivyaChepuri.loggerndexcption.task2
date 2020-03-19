c4b9c8c484daf1c4caf4936e8c59ef619fc98b6a
Divya203 <divyachepuri203@gmail.com>
2020-03-19 19:29:59 +0530

    loggerandexception task2

---
+++ src/main/java/com/divya/logger/exception/MaterialsCost.java
@@ -0,0 +1,9 @@
+package com.divya.logger.exception;
+
+interface MaterialsCost {
+	Integer standardMaterials();
+	Integer aboveStandardMaterials();
+	Integer highStandardMaterials();
+	Integer getConstructionCost();
+
+}
